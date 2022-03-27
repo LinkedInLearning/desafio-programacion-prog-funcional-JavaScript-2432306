c39f2d5f1a731f179200c01cab250f6b0629f12e
Natalia C <46870371+nlunaty@users.noreply.github.com>
2022-03-27 23:47:36 +0100

    01_i

---
+++ 01_marcador_tiempo/ejercicio/js/index.js
@@ -0,0 +1,8 @@
+
+// Escribir un cronómetro utilizando RxJS
+// https://www.learnrxjs.io/
+
+const resultado = document.getElementById('resultado');
+
+const { timer } = rxjs;
+const { map } = rxjs.operators;
